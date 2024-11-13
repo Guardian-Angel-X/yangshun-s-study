b4e1aa4424ae26474a059992a199556a1996d4f2
Guardian-Angel-X <479746669@qq.com>
2024-11-13 21:01:59 +0800

    Add files via upload

---
+++ pair对组的创建.cpp
@@ -0,0 +1,11 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	pair<string, int> p1("杨书润", 18);
+//	pair<string, int> p2("郭安昕", 17);
+//
+//	cout << "姓名：" << p1.first << " 年龄：" << p1.second << endl;
+//	cout << "姓名：" << p2.first << " 年龄：" << p2.second << endl;
+//}
---
+++ set容器的排序.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<set>
+using namespace std;
+
+//¹¹Ôì·Âº¯Êı
+class MyCompare
+{
+public:
+	bool operator()(int v1, int v2)const
+	{
+		return v1 > v2;
+	}
+};
+
+void Print(set<int>&s)
+{
+	for (set<int>::iterator it = s.begin(); it != s.end(); it++)
+	{
+		cout << *it << " ";
+	}
+	cout << endl;
+}
+
+int main()
+{
+	set<int>s1;
+
+	s1.insert(10);
+	s1.insert(30);
+	s1.insert(40);
+	s1.insert(20);
+	s1.insert(50);
+	Print(s1);
+
+	set<int,MyCompare>s2;
+	s2.insert(10);
+	s2.insert(30);
+	s2.insert(40);
+	s2.insert(20);
+	s2.insert(50);
+	for (set<int, MyCompare>::iterator it = s2.begin(); it != s2.end(); it++)
+	{
+		cout << *it << " ";
+	}
+	cout << endl;
+}
